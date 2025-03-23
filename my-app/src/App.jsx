453750f21f470e04d5d0f15c0ddf9032266a1f7e
Dhananjay Shinde <shinde3270@gmail.com>
2025-03-23 20:03:42 +0530

    Infinite_Scroll

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [items,setItems] = useState( [...new Array(40)]);
+
+function hitmore() {
+  setTimeout(() => {
+    setItems(prev => [...prev, ...new Array(10)]);
+  }, 1000);
+}
+function handleScroll(e){
+  const ScrollTop = e.target.scrollTop
+  const clientHeight = e.target.clientHeight
+  const scrollHeight = e.target.scrollHeight
+const remaningScroll = scrollHeight - (ScrollTop  + clientHeight)
+console.log(remaningScroll)
+if(remaningScroll<100)
+{
+  hitmore()
+}
+}
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div  onScroll={(e)=>handleScroll(e)} className='container'>
+      {
+        items.map((val , index)=> {
+          return (<div className='item' key={index}>{index+1}</div>)
+        })
+      }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
